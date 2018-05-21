5463517b1d12c87fd67b21e81a2f003a52aec01d
Yukine <blazylp@gmail.com>
2018-05-21 20:21:55 +0200

    added linter config and adjusted code to new rules

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -5,21 +5,21 @@ import { escape } from 'querystring';
 import { get } from 'snekfetch';
 
 export interface DefaultWeatherOptions {
-	language?: string,
-	degreeType?: string
+	language?: string;
+	degreeType?: string;
 }
 
 export interface WeatherOptions extends DefaultWeatherOptions {
-	query: string
+	query: string;
 }
 
 export class Client {
-	private defLanguage: string = 'en-us';
-	private defDegreeType: string = 'C';
+	private defLanguage = 'en-us';
+	private defDegreeType = 'C';
 
 	public constructor(options: DefaultWeatherOptions = {}) {
-		if(options.language) this.defLanguage = options.language;
-		if(options.degreeType) this.defDegreeType = options.degreeType;
+		if (options.language) this.defLanguage = options.language;
+		if (options.degreeType) this.defDegreeType = options.degreeType;
 	}
 
 	public async getWeather(options: WeatherOptions): Promise<Weather[]> {
@@ -38,10 +38,10 @@ export class Client {
 				if (err) return reject(err);
 
 				if (result.weatherdata.weather.$ && result.weatherdata.weather.$.errormessage) return reject(new Error(result.weatherdata.weather.$.errormessage));
-				
-				if(!result || !result.weatherdata || !result.weatherdata.weather) return reject(new Error('failed to parse weather data'));
-				
-				if(!(result.weatherdata.weather instanceof Array)) return reject(new Error('missing weather info'));
+
+				if (!result || !result.weatherdata || !result.weatherdata.weather) return reject(new Error('failed to parse weather data'));
+
+				if (!(result.weatherdata.weather instanceof Array)) return reject(new Error('missing weather info'));
 
 				const parsedWeather: Array<Weather> = [];
 				for (const weather of result.weatherdata.weather) {
@@ -50,7 +50,7 @@ export class Client {
 				}
 
 				return resolve(parsedWeather);
-			})
-		})
+			});
+		});
 	}
 }
--- src/structures/Weather.ts
+++ src/structures/Weather.ts
@@ -1,41 +1,41 @@
 export interface Current {
-	temperature: string,
-	skycode: string,
-	skytext: string,
-	date: string,
-	observationtime: string,
-	observationpoint: string,
-	feelslike: string,
-	humidity: string,
-	winddisplay: string,
-	day: string,
-	shortday: string,
-	windspeed: string,
-	imageUrl: string
+	temperature: string;
+	skycode: string;
+	skytext: string;
+	date: string;
+	observationtime: string;
+	observationpoint: string;
+	feelslike: string;
+	humidity: string;
+	winddisplay: string;
+	day: string;
+	shortday: string;
+	windspeed: string;
+	imageUrl: string;
 }
 
-export interface forecast {
-	low: string,
-	high: string,
-	skycodeday: string,
-	skytextday: string,
-	date: string,
-	day: string,
-	shortday: string,
-	precip: string
+export interface Forecast {
+	low: string;
+	high: string;
+	skycodeday: string;
+	skytextday: string;
+	date: string;
+	day: string;
+	shortday: string;
+	precip: string;
 }
 
 export default class Weather {
-	public name: string
-	public zipcode: string
-	public lat: string
-	public long: string
-	public timezone: string
-	public alert: string
-	public degreetype: string
-	public imagerelativeurl: string
+	public name: string;
+	public zipcode: string;
+	public lat: string;
+	public long: string;
+	public timezone: string;
+	public alert: string;
+	public degreetype: string;
+	public imagerelativeurl: string;
 	public current: Current | null = null;
-	public forecast: forecast[] = [];
+	public forecast: Forecast[] = [];
 
 	public constructor(data: any) {
 		const realData = data.$;
@@ -51,11 +51,11 @@ export default class Weather {
 
 		if (data.current instanceof Array && data.current.length > 0) {
 			if (typeof data.current[0].$ === 'object') {
-			  this.current = data.current[0].$ as Current;
-			  this.current.imageUrl = `${this.imagerelativeurl}law/${this.current.skycode}.gif`;
+				this.current = data.current[0].$ as Current;
+				this.current.imageUrl = `${this.imagerelativeurl}law/${this.current.skycode}.gif`;
 			}
 		}
-		
+
 		if (data.forecast instanceof Array) {
 			for (const forecast of data.forecast) {
 				if (typeof forecast === 'object') this.forecast.push(forecast);
